420ee3085642cf583281415f75c2a0863421dbca
James Stevenson <jamesstevenson@laurens-MacBook-Air.local>
2019-10-30 21:00:18 +0000

    spiking with scryfall data, should be able to generate dev data soon

---
+++ index.js
@@ -0,0 +1,24 @@
+const { readFile } = require("fs");
+
+readFile("./db/data/scryfall-cards.json", "utf8", (err, data) => {
+  workingData = JSON.parse(data);
+
+  onlyM20 = workingData.filter(card => {
+    return card.set === "m20" && card.rarity === "rare";
+  });
+
+  onlym20RareCardNames = onlyM20.map(
+    ({ name, rarity, mana_cost, oracle_text, image_uris }) => {
+      let returningCardObj = {
+        name,
+        rarity,
+        mana_cost,
+        oracle_text,
+        image: image_uris.normal
+      };
+      return returningCardObj;
+    }
+  );
+
+  console.log(onlym20RareCardNames);
+});
